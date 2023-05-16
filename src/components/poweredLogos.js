c28d96d9362ede4aee82c77661010465257403e3
danmondra <danimonta15@gmail.com>
2023-05-16 01:02:23 -0600

    Add lazy loading to images

--- src/components/poweredLogos.js
+++ src/components/poweredLogos.js
@@ -4,16 +4,16 @@ export function PoweredLogos() {
   return (
     <div className={styles.poweredByGrid}>
       <a href='https://infojobs.net' target='_blank' rel='noopener noreferer'>
-        <img src='/infojobs.png' alt='Logo of infojobs' />
+        <img src='/infojobs.png' alt='Logo of infojobs' lazy='loading' />
       </a>
       <a href='https://midu.dev' target='_blank' rel='noopener noreferer'>
-        <img src='/midudev.png' alt='Logo of Midu.dev' />
+        <img src='/midudev.png' alt='Logo of Midu.dev' lazy='loading' />
       </a>
       <a href='https://openai.com' target='_blank' rel='noopener noreferer'>
-        <img src='/openAi.png' alt='Logo of openAi' />
+        <img src='/openAi.png' alt='Logo of openAi' lazy='loading' />
       </a>
       <a href='https://cohere.com' target='_blank' rel='noopener noreferer'>
-        <img src='/cohere.png' alt='Logo of Cohere' />
+        <img src='/cohere.png' alt='Logo of Cohere' lazy='loading' />
       </a>
     </div>
   )

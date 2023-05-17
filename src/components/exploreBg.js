05e18061badb01c9c1f4ed40b5646aed14cee882
danmondra <danimonta15@gmail.com>
2023-05-16 21:04:59 -0600

    Add background to 'explore' page

---
+++ src/components/exploreBg.js
@@ -0,0 +1,14 @@
+import styles from '@/styles/explore.module.css'
+
+export function ExploreBG() {
+  return (
+    <div className={styles.pageBg}>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+}

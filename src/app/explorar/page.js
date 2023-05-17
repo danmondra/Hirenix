5a216a6f143e47388b15605d87716d790916a4b6
danmondra <danimonta15@gmail.com>
2023-05-17 13:34:04 -0600

    Improve images size

--- src/app/explorar/page.js
+++ src/app/explorar/page.js
@@ -2,6 +2,7 @@ import styles from '@/styles/explore.module.css'
 import { ExploreBG } from '@/components/exploreBg'
 import { Aside } from '@/components/aside'
 import { NavigationLinks } from '@/components/navigationLinks'
+import { Footer } from '@/components/footer'
 
 export default function Explore() {
   return (
@@ -9,12 +10,13 @@ export default function Explore() {
       <ExploreBG />
       <div className={styles.page}>
         <Aside />
-        <main className={styles.main}>
+        <main className={`exploreMain ${styles.main}`}>
           <header className='exploreHeader'>
             <nav className='navigation'>
               <NavigationLinks />
             </nav>
           </header>
+          <Footer />
         </main>
       </div>
     </>

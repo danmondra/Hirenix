214d3c99e7203e72b3be72565b7306bb1f974000
danmondra <danimonta15@gmail.com>
2023-05-24 02:22:19 -0600

    Add component to select offer when search

--- src/app/(offers)/search/page.js
+++ src/app/(offers)/search/page.js
@@ -1,18 +1,34 @@
 import { Filters } from '@/components/filters'
 import styles from '@/styles/search.module.css'
 import { CardOffer } from '@/components/cardOffer'
+import { CardOfferSelect } from '@/components/cardOfferSelect'
 
-export default function Search() {
+export default function Search({ searchParams }) {
+  console.log(searchParams)
+
   return (
     <section className={`containerExplore ${styles.search}`}>
       <header>
         <h1 className={styles.title}><span>302 Ofertas</span> para “Desarrollador Web”</h1>
         <p className={styles.text}>Mostrando 10 resultados de 302</p>
         <Filters />
-        <CardOffer />
-        <CardOffer />
-        <CardOffer />
       </header>
+      <section className={styles.offersContainer}>
+        {
+          searchParams?.select
+            ? <>
+              <CardOfferSelect />
+              <CardOfferSelect />
+              <CardOfferSelect />
+              </>
+            : <>
+              <CardOffer />
+              <CardOffer />
+              <CardOffer />
+              </>
+
+        }
+      </section>
     </section>
   )
 }

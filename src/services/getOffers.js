fc37bcebe054e0a8f725982e633d7eae06f9ba97
danmondra <danimonta15@gmail.com>
2023-05-25 20:48:25 -0600

    Change cache options when get offers

--- src/services/getOffers.js
+++ src/services/getOffers.js
@@ -5,13 +5,15 @@ const credentials = `${clientId}:${clientSecret}`
 const encodedCredentials = Buffer.from(credentials).toString('base64')
 
 export async function getInfojobsOffers(page = 1, maxResults = 15) {
+  // TODO --- Disable cache, try with cache: 'no-cache' o 'no-store'
   const URLTransformed = `${URL}?page=${page}&maxResults=${maxResults}`
 
   const res = await fetch(URLTransformed, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Basic ${encodedCredentials}`
-    }
+    },
+    cache: 'no-cache'
   })
 
   return await res.json()
@@ -22,6 +24,10 @@ export async function getOfferById(id) {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Basic ${encodedCredentials}`
+    },
+    cache: 'force-cache',
+    next: {
+      revalidate: 3600
     }
   })
 

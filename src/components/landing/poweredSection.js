70e9a09521bd59221241c86d9d3c883fcfcdb04d
danmondra <danimonta15@gmail.com>
2023-05-27 01:54:51 -0600

    Fix error CSS module error when navigating to new segment from home

--- src/components/landing/poweredSection.js
+++ src/components/landing/poweredSection.js
@@ -1,12 +1,13 @@
 import styles from '@/styles/landingPage.module.css'
 import searchStyles from '@/styles/search.module.css'
+import offerStyles from '@/styles/offer.module.css'
 import { PoweredLogos } from '@/components/landing/poweredLogos'
 import { MainLink } from '@/components/landing/mainLink'
 
 export function PoweredSection() {
   return (
     <section className={styles.powered}>
-      <span className={searchStyles.toLoadSearchStylesAndAvoidError} />
+      <span className={`${searchStyles.toLoadSearchStylesAndAvoidError} ${offerStyles.toLoadSearchStylesAndAvoidError}`} />
       <div className={styles.poweredBg} />
       <div className={`container ${styles.poweredContainer}`}>
         <div className={styles.poweredTopContainer}>

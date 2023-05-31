9e1e80e5f872e14638716ff3773aaadf87dfb86b
danmondra <danimonta15@gmail.com>
2023-05-31 00:47:55 -0600

    Fix error when stringify json

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -5,21 +5,19 @@ const clientSecret = process.env.INFOJOBS_CLIENTSECRET
 const clientId = process.env.INFOJOBS_CLIENTID
 
 export async function GET(req) {
-  console.log('entró')
   const { searchParams } = new URL(req.url)
   const code = searchParams.get('code')
-  console.log(code)
-  console.log({ redirectUri, clientSecret, clientId })
 
   try {
     const res = await fetch(`https://www.infojobs.net/oauth/authorize?grant_type=authorization_code&client_id=${clientId}&client_secret=${encodeURI(clientSecret)}&code=${code}&redirect_uri=${redirectUri}`, {
       method: 'POST'
     })
     const token = await res.json()
-    console.log({ token })
-    const tokenEncoded = JSON.stringify(encodeURI(token))
+    console.log(token)
 
-    const response = NextResponse.json({ msg: 'success' })
+    const tokenEncoded = JSON.stringify(encodeURIComponent(token))
+
+    const response = NextResponse.json(token)
     response.cookies.set({
       name: 'userTokenInfojobs',
       value: tokenEncoded,
